fb1f0dd91c612d24c1a329992296b07380fc85d0
David Sousa <62266135+ctrl-afk@users.noreply.github.com>
2022-04-21 10:11:59 -0300

    New lib. Criando conexão, recebendo e enviando textos.

--- src/main/java/com/unip/aps_msger/ApsMsgerApplication.java
+++ src/main/java/com/unip/aps_msger/ApsMsgerApplication.java
@@ -1,20 +1,11 @@
 package com.unip.aps_msger;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories @Configuration
 @SpringBootApplication
 public class ApsMsgerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApsMsgerApplication.class, args);
-    }
-
-    @Autowired
-    public ApsMsgerApplication() {
-        new WebsocketServer().start();
     }
 }
--- src/main/java/com/unip/aps_msger/WebsocketServer.java
+++ src/main/java/com/unip/aps_msger/WebsocketServer.java
@@ -1,95 +1,30 @@
 package com.unip.aps_msger;
 
-import com.unip.aps_msger.model.MessageDto;
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpRequest;
-import java.nio.ByteBuffer;
-import java.util.*;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
-@Configuration
-public class WebsocketServer extends WebSocketServer {
-    private static final int TCP_PORT = 4449;
-    Set<WebSocket> connections;
-
-    @Autowired
-    MsgService service;
-
-
-    @Autowired
-    public WebsocketServer() {
-        super(new InetSocketAddress(TCP_PORT));
+@Component @ServerEndpoint(value = "/")
+public class WebsocketServer {
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+        System.out.println("Welcome! New session open : " + session);
+        System.out.println("Used endpointConfig : " + endpointConfig + "\n");
     }
 
-    @Override
-    public void onOpen(WebSocket connection, ClientHandshake handshake) {
-        connection.send("Welcome! Log in success.");
-        connections.add(connection);
-        System.out.println("New connection from " + connection.getRemoteSocketAddress().getAddress().getHostAddress());
-        broadcast("New connection : " + handshake.getResourceDescriptor());
-    }
-
-    @Override
-    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        connections.remove(conn);
-        System.out.println("Closed connection to " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
-        System.out.println("Host : " + conn.getLocalSocketAddress().getHostName());
-        System.out.println("Port : " + conn.getLocalSocketAddress().getPort());
-    }
-
-    @Override
-    public void onMessage(WebSocket webSocket, String s) {
-        broadcast(s);
-        System.out.println(webSocket + " ==> " + s);
-
-        /*
-        new Thread("POST") {
-            @Override
-            public void run() {
-                super.run();
-                try {
-                // Tentativa 1
-                // Usar o service para salvar diretamente no bd
-                service.save(new MessageDto(1L, s));
-
-                // Tentativa 2
-                // Usar um request para postar no bd que está rodando em outra porta
-                    HttpRequest.newBuilder()
-                            .uri(new URI("http://localhost:8080/api"))
-                            .headers("Content-Type", "text/plain;charset=UTF-8")
-                            .POST(HttpRequest.BodyPublishers.ofString(s))
-                            .build();
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                }
+    @OnMessage
+    public void onMessage(Session session, String msg) {
+        System.out.println(session + " :: " + msg  + "\n");
+        try {
+            for (Session sess : session.getOpenSessions()) {
+                if (sess.isOpen())
+                    sess.getBasicRemote().sendText(msg);
             }
-        }.start();
-         */
-    }
-
-    @Override
-    public void onMessage(WebSocket conn, ByteBuffer message) {
-        broadcast(message.array());
-        System.out.println(conn + " ==> " + message);
-    }
-
-    @Override
-    public void onError(WebSocket conn, Exception ex) {
-        ex.printStackTrace();
-        if (conn != null) {
-            System.out.println("ERROR from " + conn);
+        } catch (IOException e) {
+            System.out.println("Error to send message!");
+            e.printStackTrace();
         }
-    }
-
-    @Override
-    public void onStart() {
-        System.out.println("Server started!");
     }
 }
