a85e62140ac3d1932c6ae289aac9e703a89f32f6
David Sousa <62266135+ctrl-afk@users.noreply.github.com>
2022-04-22 09:12:16 -0300

    Websocket e db agora são separados

--- src/main/java/com/unip/aps_msger/WebsocketServer.java
+++ src/main/java/com/unip/aps_msger/WebsocketServer.java
@@ -2,21 +2,23 @@ package com.unip.aps_msger;
 
 import org.springframework.stereotype.Component;
 
-import javax.websocket.*;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 
 @Component @ServerEndpoint(value = "/")
 public class WebsocketServer {
     @OnOpen
-    public void onOpen(Session session, EndpointConfig endpointConfig) {
-        System.out.println("Welcome! New session open : " + session);
-        System.out.println("Used endpointConfig : " + endpointConfig + "\n");
+    public void onOpen(Session session) {
+        System.out.println("\nWelcome! New session open : " + session);
     }
 
     @OnMessage
     public void onMessage(Session session, String msg) {
-        System.out.println(session + " :: " + msg  + "\n");
+        System.out.println(session.getId() + " :: " + msg  + "\n");
         try {
             for (Session sess : session.getOpenSessions()) {
                 if (sess.isOpen())
@@ -26,5 +28,11 @@ public class WebsocketServer {
             System.out.println("Error to send message!");
             e.printStackTrace();
         }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        System.out.println("Error from session" + session.getId());
+        throwable.printStackTrace();
     }
 }
--- src/main/java/com/unip/aps_msger/WsConfig.java
+++ src/main/java/com/unip/aps_msger/WsConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+
 @Configuration @EnableWebSocket
 public class WsConfig {
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
+
 }
