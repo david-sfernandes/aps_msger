56c555011a4d1220b4a2a34b14382a5863be3d8a
David Sousa <62266135+ctrl-afk@users.noreply.github.com>
2022-04-22 09:28:59 -0300

    Websocket e db agora são separados

--- src/main/java/com/unip/aps_msger/ApsMsgerApplication.java
+++ src/main/java/com/unip/aps_msger/ApsMsgerApplication.java
@@ -3,10 +3,7 @@ package com.unip.aps_msger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories @Configuration
 @SpringBootApplication
 public class ApsMsgerApplication {
     public static void main(String[] args) {
--- src/main/java/com/unip/aps_msger/WebsocketServer.java
+++ src/main/java/com/unip/aps_msger/WebsocketServer.java
@@ -1,6 +1,5 @@
 package com.unip.aps_msger;
 
-import com.unip.aps_msger.model.MessageDto;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
@@ -8,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpRequest;
 import java.nio.ByteBuffer;
 import java.util.*;
 
@@ -18,10 +14,6 @@ import java.util.*;
 public class WebsocketServer extends WebSocketServer {
     private static final int TCP_PORT = 4449;
     Set<WebSocket> connections;
-
-    @Autowired
-    MsgService service;
-
 
     @Autowired
     public WebsocketServer() {
@@ -48,30 +40,6 @@ public class WebsocketServer extends WebSocketServer {
     public void onMessage(WebSocket webSocket, String s) {
         broadcast(s);
         System.out.println(webSocket + " ==> " + s);
-
-        /*
-        new Thread("POST") {
-            @Override
-            public void run() {
-                super.run();
-                try {
-                // Tentativa 1
-                // Usar o service para salvar diretamente no bd
-                service.save(new MessageDto(1L, s));
-
-                // Tentativa 2
-                // Usar um request para postar no bd que está rodando em outra porta
-                    HttpRequest.newBuilder()
-                            .uri(new URI("http://localhost:8080/api"))
-                            .headers("Content-Type", "text/plain;charset=UTF-8")
-                            .POST(HttpRequest.BodyPublishers.ofString(s))
-                            .build();
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-         */
     }
 
     @Override
